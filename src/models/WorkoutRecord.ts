131cba4b8d139920dfae83b69f4c98e10efe4ef2
Infant Ajay Venus <infantcsajay@outlook.com>
2023-06-10 12:48:24 +0530

    [model] fix: remove redundant workout field in WorkoutTrackRecord

--- src/models/WorkoutRecord.ts
+++ src/models/WorkoutRecord.ts
@@ -1,5 +1,5 @@
 import getRandomId, { ID } from "src/utils/getRandomId";
-import { TrackingValues, Workout } from "./Workout";
+import { TrackingValues } from "./Workout";
 
 export interface TrackedValuesData {
     id: ID,
@@ -10,14 +10,12 @@ export interface TrackedValuesData {
 
 export class WorkoutTrackRecord {
     private _id: ID;
-    private _workout: Workout;
     private _time?: Number;
     private _count?: Number;
     private _weight?: Number;
     private _timestamp: Date;
 
-    constructor(workout: Workout, { time, count, weight }: { time?: Number, count?: Number, weight?: Number } = {}, id: ID = getRandomId(), timestamp: Date = new Date()) {
-        this._workout = workout;
+    constructor({ time, count, weight }: { time?: Number, count?: Number, weight?: Number } = {}, id: ID = getRandomId(), timestamp: Date = new Date()) {
         this._id = id;
         this._timestamp = timestamp;
         this._time = time;
@@ -41,10 +39,6 @@ export class WorkoutTrackRecord {
         return this._id;
     }
 
-    get workout() {
-        return this._workout;
-    }
-
     get count() {
         return this._count;
     }
@@ -61,15 +55,15 @@ export class WorkoutTrackRecord {
         return this._timestamp;
     }
 
-    get hasAllMandatoryValues() {
-        const shouldHaveTime = this.workout.trackingValues.includes(TrackingValues.TIME);
-        const shouldHaveWeight = this.workout.trackingValues.includes(TrackingValues.WEIGHT);
-        const shouldHaveCount = this.workout.trackingValues.includes(TrackingValues.COUNT);
-
-        return (shouldHaveCount && !!this.count) || (shouldHaveWeight && !!this.weight) || (shouldHaveTime && !!this.time);
-    }
-
     get formattedDate() {
         return `${this._timestamp.getDate()}/${this._timestamp.getMonth() + 1}/${this._timestamp.getFullYear()}`;
+    }
+
+    getHasAllMandatoryValues = (trackingValues: TrackingValues[]) => {
+        const shouldHaveTime = trackingValues.includes(TrackingValues.TIME);
+        const shouldHaveWeight = trackingValues.includes(TrackingValues.WEIGHT);
+        const shouldHaveCount = trackingValues.includes(TrackingValues.COUNT);
+
+        return (shouldHaveCount && !!this.count) || (shouldHaveWeight && !!this.weight) || (shouldHaveTime && !!this.time);
     }
 }
