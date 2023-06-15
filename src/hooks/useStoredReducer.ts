756ad776e21cff7e47d9e4c8d74cd9d2228566cb
Infant Ajay Venus <infantcsajay@outlook.com>
2023-06-15 07:38:40 +0530

    [hooks] feat: implement useSToredReducer

---
+++ src/hooks/useStoredReducer.ts
@@ -0,0 +1,30 @@
+import { get, set, update } from "idb-keyval";
+import { Dispatch, useEffect, useReducer } from "react";
+
+export default function useStoredReducer<S, A>(
+    storeKey: IDBValidKey, 
+    reducer: (state: S, action: A) => S, 
+    initialState: S, 
+    initializeActionGenerator: (restoreState:S) => A
+):[S, Dispatch<A>] {
+    const [state, dispatch] = useReducer((state: S, action: A) => {
+        const updatedState = reducer(state, action);
+        update(storeKey, () => updatedState);
+        return updatedState;
+    }, initialState);
+    
+
+    useEffect(() => {
+        (async () => {
+            const storedData = await get(storeKey);
+            if (!storedData) {
+                set(storeKey, initialState);
+            }else {
+                console.log("DEBUG:STORE_DATA_RESTORED")
+                dispatch(initializeActionGenerator(storedData));
+            }
+        })();
+    }, [])
+
+    return [state as S, dispatch as Dispatch<A>];
+}
