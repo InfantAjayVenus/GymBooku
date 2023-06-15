3086e7124905642da57521d340949acf42c363e3
Infant Ajay Venus <infantcsajay@outlook.com>
2023-06-15 18:10:35 +0530

    [reducer] fix: add changes to planreducer enabling state restore

--- src/reducers/PlanReducer.ts
+++ src/reducers/PlanReducer.ts
@@ -1,6 +1,7 @@
 import { Plan } from "src/models/Plan";
 
 export enum PlanActionType {
+    INIT_PLAN = "INIT_PLAN",
     ADD_PLAN = "ADD_PLAN",
     UPDATE_PLAN = "UPDATE_PLAN",
     DELETE_PLAN = "DELETE_PLAN",
@@ -8,20 +9,30 @@ export enum PlanActionType {
 
 export interface PlanAction {
     type: PlanActionType,
-    payload: Plan
+    payload: Plan[]
 };
 
 export default function planReducer(state: Plan[], action: PlanAction) {
     switch (action.type) {
+        case PlanActionType.INIT_PLAN:
+            const restoreState = action.payload.map(planItem => {
+                if('id' in planItem) return planItem;
+
+                const rawJSON = JSON.parse(JSON.stringify(planItem));
+                return new Plan(rawJSON._name, rawJSON._workoutsList, rawJSON._daysList, rawJSON._id);
+            })
+            return restoreState;
         case PlanActionType.ADD_PLAN:
-            state = [...state, action.payload];
+            state = [...state, ...action.payload];
             return state;
         case PlanActionType.UPDATE_PLAN:
-            const updatedPlanIndex = state.findIndex(({id}) => action.payload.id === id);
-            state[updatedPlanIndex] = action.payload;
+            action.payload.forEach(planItem => {
+                const updatedPlanIndex = state.findIndex(({id}) => planItem.id === id);
+                state[updatedPlanIndex] = planItem;
+            })
             return [...state];
         case PlanActionType.DELETE_PLAN:
-            state = state.filter(({id}) => id !== action.payload.id);
+            state = state.filter(({id}) => !action.payload.some(planItem => planItem.id === id));
             return state;
         default:
             return state;
