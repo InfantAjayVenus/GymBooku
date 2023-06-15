b115ae6e6486168fbc2feb89e047d0ee0c0a4f98
Infant Ajay Venus <infantcsajay@outlook.com>
2023-06-15 07:52:58 +0530

    [reducer] feat: added new actions to workoutreducer to enable state restore from json

--- src/reducers/WorkoutReducer.ts
+++ src/reducers/WorkoutReducer.ts
@@ -1,6 +1,7 @@
 import { Workout } from "src/models/Workout";
 
 export enum WorkoutActionType {
+    INIT_WORKOUT = "INIT_WORKOUT",
     ADD_WORKOUT = "ADD_WORKOUT",
     UPDATE_WORKOUT = "UPDATE_WORKOUT",
     DELETE_WORKOUT = "DELETE_WORKOUT",
@@ -8,20 +9,31 @@ export enum WorkoutActionType {
 
 export interface WorkoutAction {
     type: WorkoutActionType,
-    payload: Workout
+    payload: Workout[]
 };
 
 export default function workoutReducer(state: Workout[], action: WorkoutAction) {
     switch (action.type) {
+        case WorkoutActionType.INIT_WORKOUT:
+            const restoreState = action.payload.map(stateItem => {
+                if('id' in stateItem) return stateItem;
+
+                const rawJSON = JSON.parse(JSON.stringify(stateItem));
+                return new Workout(rawJSON._workoutName, rawJSON._trackingValues, rawJSON._id);
+            })
+            
+            return restoreState;
         case WorkoutActionType.ADD_WORKOUT:
-            state = [...state, action.payload];
+            state = [...state, ...action.payload];
             return state;
         case WorkoutActionType.UPDATE_WORKOUT:
-            const updatedWorkoutIndex = state.findIndex(({id}) => action.payload.id === id);
-            state[updatedWorkoutIndex] = action.payload;
+            action.payload.forEach(updateItem => {
+                const updatedWorkoutIndex = state.findIndex(({ id }) => updateItem.id === id);
+                state[updatedWorkoutIndex] = updateItem;
+            })
             return [...state];
         case WorkoutActionType.DELETE_WORKOUT:
-            state = state.filter(({id}) => id !== action.payload.id);
+            state = state.filter(({ id }) => !action.payload.some(deleteItem => deleteItem.id === id));
             return state;
         default:
             return state;
