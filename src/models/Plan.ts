4dc17e17d1f333c9a6890f35e79d7fd4e3c82f17
Infant Ajay Venus <infantcsajay@outlook.com>
2023-06-15 07:55:10 +0530

    [model] feat: add optional ID to plan constructor to enable copying

--- src/models/Plan.ts
+++ src/models/Plan.ts
@@ -17,8 +17,8 @@ export class Plan {
     private _workoutsList: Workout[];
     private _daysList: DAYS_OF_WEEK[];
 
-    constructor(name:String, workoutsList: Workout[], daysList: DAYS_OF_WEEK[]) {
-        this._id = getRandomId();
+    constructor(name:String, workoutsList: Workout[], daysList: DAYS_OF_WEEK[], id: ID = getRandomId()) {
+        this._id = id;
         this._name = name;
         this._workoutsList = workoutsList;
         this._daysList = daysList;
