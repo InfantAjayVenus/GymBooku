95b4306cc8bb0766cda798734d1783e99374cf6c
Infant Ajay Venus <infantcsajay@outlook.com>
2023-06-16 15:01:04 +0530

    [useStoredReducer] feat: add clean up before storing

--- src/hooks/useStoredReducer.ts
+++ src/hooks/useStoredReducer.ts
@@ -2,28 +2,53 @@ import { get, set, update } from "idb-keyval";
 import { Dispatch, useEffect, useReducer } from "react";
 
 export default function useStoredReducer<S, A>(
-    storeKey: IDBValidKey, 
-    reducer: (state: S, action: A) => S, 
-    initialState: S, 
-    initializeActionGenerator: (restoreState:S) => A
-):[S, Dispatch<A>] {
+    storeKey: IDBValidKey,
+    reducer: (state: S, action: A) => S,
+    initialState: S,
+    initializeActionGenerator: (restoreState: S) => A
+): [S, Dispatch<A>] {
     const [state, dispatch] = useReducer((state: S, action: A) => {
-        const updatedState = reducer(state, action);
-        update(storeKey, () => updatedState);
-        return updatedState;
+        const reducedState = reducer(state, action);
+
+        const updatedState = getDeepCleanedObject(reducedState as Object);
+        console.log(`DEBUG:${storeKey}`, updatedState);
+
+        update(storeKey, () => updatedState)
+            .catch(err => console.log(storeKey, updatedState, err));
+        return reducedState;
     }, initialState);
-    
+
 
     useEffect(() => {
         (async () => {
             const storedData = await get(storeKey);
             if (!storedData) {
                 set(storeKey, initialState);
-            }else {
+            } else {
                 dispatch(initializeActionGenerator(storedData));
             }
         })();
     }, [])
 
     return [state as S, dispatch as Dispatch<A>];
+}
+
+function getDeepCleanedObject(object: any, debug = false): any {
+    debug && console.log('DEBUG:OBJECT:', typeof object, object instanceof Array);
+
+    if (object instanceof Array) return object.map(value => getDeepCleanedObject(value));
+    if (typeof object !== 'object') return object;
+    if (object instanceof Date) return object;
+
+    return Object.fromEntries(
+        Object.entries(object)
+            .filter(([_, value]) => (typeof value).toLowerCase() !== 'function')
+            .map(([key, value]) => {
+                if ((typeof value).toLowerCase() === 'object') return (
+                    [key, getDeepCleanedObject(value)]
+                );
+
+                return [key, value];
+            })
+    )
 }
