e6fe82a24032cc7718aa53479c060a1e79e15a17
Infant Ajay Venus <infantcsajay@outlook.com>
2023-06-24 12:27:59 +0530

    [planner] feat: add text if plans are empty

--- src/pages/WorkoutPlanner.tsx
+++ src/pages/WorkoutPlanner.tsx
@@ -51,65 +51,74 @@ function WorkoutPlanner({ values, workoutsList, onAdd, onDelete, onUpdate }: Wor
         <>
             <Stack padding={4} spacing={2} position={'relative'}>
                 <Typography variant="h5" fontWeight={'bold'} component={'h3'}>Workout Plans</Typography>
-                <List>
-                    {values.map((planItem) => (
-                        <Fragment key={planItem.id as Key}>
-                            <ListItem
-                                key={planItem.id as Key}
-                                sx={{
-                                    paddingLeft: '0',
-                                    flexDirection: 'column',
-                                    alignItems: 'flex-start',
-                                    textAlign: 'left'
-                                }}
-                                secondaryAction={
-                                    <IconButton
-                                        aria-label="more"
-                                        id="long-button"
-                                        aria-controls={menuPopover.isOpen ? 'long-menu' : undefined}
-                                        aria-expanded={menuPopover.isOpen ? 'true' : undefined}
-                                        aria-haspopup="true"
-                                        onClick={(event) => {
-                                            setCurrentPlanId(planItem.id);
-                                            setMenuAnchorElement(event.currentTarget);
-                                            menuPopover.open();
-                                        }}
-                                    >
-                                        <MoreVert />
-                                    </IconButton>
-                                }
-                            >
-                                <ListItemText
-                                    primary={(
-                                        <Typography variant="h6">{planItem.name}</Typography>
-                                    )}
-                                />
-                                <Stack>
-                                    <Typography variant="body1">
-                                        {planItem.workoutsList.map((workoutItem) => (workoutsList.find(({ id }) => workoutItem === id))?.name || false).filter(name => !!name).join(' / ')}
-                                    </Typography>
-                                    <Container component={'div'} sx={{
-                                        flexDirection: 'row',
-                                        alignContent: 'space-around',
-                                        padding: '0.3rem 0 !important',
-                                    }}>
-                                        {planItem.daysList.map((day, index) => (
-                                            <Chip
-                                                key={index}
-                                                label={day.slice(0, 3)}
-                                                sx={{
-                                                    marginRight: '0.3rem',
-                                                    marginBottom: '0.3rem',
-                                                }}
-                                            />
-                                        ))}
-                                    </Container>
-                                </Stack>
-                            </ListItem>
-                            <Divider />
-                        </Fragment>
-                    ))}
-                </List>
+                {values.length > 0 && (
+                    <List>
+                        {values.map((planItem) => (
+                            <Fragment key={planItem.id as Key}>
+                                <ListItem
+                                    key={planItem.id as Key}
+                                    sx={{
+                                        paddingLeft: '0',
+                                        flexDirection: 'column',
+                                        alignItems: 'flex-start',
+                                        textAlign: 'left'
+                                    }}
+                                    secondaryAction={
+                                        <IconButton
+                                            aria-label="more"
+                                            id="long-button"
+                                            aria-controls={menuPopover.isOpen ? 'long-menu' : undefined}
+                                            aria-expanded={menuPopover.isOpen ? 'true' : undefined}
+                                            aria-haspopup="true"
+                                            onClick={(event) => {
+                                                setCurrentPlanId(planItem.id);
+                                                setMenuAnchorElement(event.currentTarget);
+                                                menuPopover.open();
+                                            }}
+                                        >
+                                            <MoreVert />
+                                        </IconButton>
+                                    }
+                                >
+                                    <ListItemText
+                                        primary={(
+                                            <Typography variant="h6">{planItem.name}</Typography>
+                                        )}
+                                    />
+                                    <Stack>
+                                        <Typography variant="body1">
+                                            {planItem.workoutsList.map((workoutItem) => (workoutsList.find(({ id }) => workoutItem === id))?.name || false).filter(name => !!name).join(' / ')}
+                                        </Typography>
+                                        <Container component={'div'} sx={{
+                                            flexDirection: 'row',
+                                            alignContent: 'space-around',
+                                            padding: '0.3rem 0 !important',
+                                        }}>
+                                            {planItem.daysList.map((day, index) => (
+                                                <Chip
+                                                    key={index}
+                                                    label={day.slice(0, 3)}
+                                                    sx={{
+                                                        marginRight: '0.3rem',
+                                                        marginBottom: '0.3rem',
+                                                    }}
+                                                />
+                                            ))}
+                                        </Container>
+                                    </Stack>
+                                </ListItem>
+                                <Divider />
+                            </Fragment>
+                        ))}
+                    </List>
+                )}
+                {values.length === 0 && (
+                    <Container sx={{textAlign: 'center', mt: '8rem !important'}}>
+                        <Typography variant="h4">🤷</Typography>
+                        <Typography variant="body1">You haven't planned for any workouts yet</Typography>
+                        <Typography variant="body2" color={'GrayText'}>Create workout plans and make your workout routine effective 💪.</Typography>
+                    </Container>
+                )}
             </Stack>
             <Box sx={{ position: "fixed", bottom: '4rem', right: '1rem' }}>
                 <Fab size="medium" color="primary" aria-label="add workout"
