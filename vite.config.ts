15242e1027f81e438ebfc6b39349ea2d74ae94dc
Infant Ajay Venus <infantcsajay@outlook.com>
2023-06-24 12:32:44 +0530

    [config] feat: enable PWA install

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa';
 import {resolve} from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VitePWA({registerType: 'autoUpdate'}),
     react(),
   ],
   resolve: {
