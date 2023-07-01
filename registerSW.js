4215606fb91a11103ca05396a197d03bf7f8dbb8
InfantAjayVenus <InfantAjayVenus@users.noreply.github.com>
2023-07-01 01:45:51 +0000

    deploy: 706773a62c8e8f3e87474fb3952e48c214247d1c

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://infantajayvenus.github.io/workout-tracker/sw.js', { scope: 'https://infantajayvenus.github.io/workout-tracker/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://infantajayvenus.github.io/GymBooku/sw.js', { scope: 'https://infantajayvenus.github.io/GymBooku/' })})}
