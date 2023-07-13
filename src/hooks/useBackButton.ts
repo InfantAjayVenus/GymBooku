67b4231b0509f561354552de9d6353bbc1f0a335
Infant Ajay Venus <infantcsajay@outlook.com>
2023-07-13 23:58:33 +0530

    [hooks] feat: implement backbutton hook

---
+++ src/hooks/useBackButton.ts
@@ -0,0 +1,21 @@
+//create a hook to track the back button event and assign handler
+
+import { useCallback, useEffect } from "react";
+
+export default function useBackButton(handler: (event?: PopStateEvent) => void): undefined {
+    const handleBack = useCallback((event?: PopStateEvent) => {
+        handler(event);
+    }, [handler]);
+
+    useEffect(() => {
+        window.addEventListener("popstate", (event: PopStateEvent) => {
+            event.preventDefault();
+            event.stopPropagation();
+
+            handleBack(event);
+        });
+        return () => {
+            window.removeEventListener("popstate", handleBack);
+        };
+    });
+}
