49294c6f3eeacb635dd212bfc2c0144a6d496574
Infant Ajay Venus <infantcsajay@outlook.com>
2023-08-03 07:08:45 +0530

    [Home] refactor: change Home page file stucture to accomodate screens

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,80 @@
+import {
+    List,
+    ListItem,
+    Stack,
+    Typography
+} from "@mui/material";
+import { Key, useState } from "react";
+import GymBookuIcon from "src/components/GymBookuIcon";
+import StreakCard from "src/components/StreakCard";
+import { WorkoutFormProps } from "src/components/WorkoutForm";
+import usePlannedWorkoutsList from "src/hooks/usePlannedWorkoutsList";
+import useStreakData from "src/hooks/useStreakData";
+import { Plan } from "src/models/Plan";
+import { Workout } from "src/models/Workout";
+import WorkoutTrackerScreen from "./WorkoutTrackerScreen";
+
+type onAddType = WorkoutFormProps['onSave'];
+
+interface HomeProps {
+    workoutsList: Workout[];
+    plansList: Plan[];
+    onAdd?: onAddType;
+    onUpdate: onAddType;
+    onDelete?: onAddType;
+}
+
+function Home({ workoutsList, plansList, onUpdate }: HomeProps) {
+    const [selectedWorkout, setSelectedWorkout] = useState<Workout | null>(null);
+
+    const streakData = useStreakData(workoutsList);
+    const plannedWorkouts = usePlannedWorkoutsList(workoutsList, plansList);
+
+    return (
+        <>
+            <Stack padding={4} spacing={2}>
+                <Stack direction={'row'}>
+                    <GymBookuIcon />
+                    <Typography variant="h5" fontWeight={'bold'} component={'h3'}>GymBooku</Typography>
+                </Stack>
+                <StreakCard {...streakData} />
+                <Typography variant="body1" fontWeight={'bold'}>Today's Workouts</Typography>
+
+                <List>
+                    {plannedWorkouts.map((workoutItem) => {
+                        return (
+                            <ListItem
+                                key={workoutItem.id as Key}
+                                sx={{
+                                    py: '1rem',
+                                    borderBottom: '1px gray solid'
+                                }}
+                                onClick={() => setSelectedWorkout(workoutItem)}
+                            >
+                                <Typography variant="body1">{workoutItem.name}</Typography>
+                            </ListItem>
+                        )
+                    }).filter((element): element is JSX.Element => !!element)}
+                </List>
+            </Stack>
+            <WorkoutTrackerScreen
+                selectedWorkout={selectedWorkout}
+                onSave={(updatedWorkoutTrackCollection) => {
+                    if (!selectedWorkout) return;
+                    if (updatedWorkoutTrackCollection?.trackedData.some(item => !item.hasAllRequiredValues(selectedWorkout.trackingValues))) return;
+                    updatedWorkoutTrackCollection && selectedWorkout.workoutTrackData.push(updatedWorkoutTrackCollection);
+                    onUpdate(Workout.copyFrom(selectedWorkout));
+                }}
+                onDelete={(deletedWorkoutTrackCollection) => {
+                    if(!selectedWorkout) return;
+                    if (!selectedWorkout.workoutTrackData.find(({ id }) => id === deletedWorkoutTrackCollection?.id)) return;
+                    selectedWorkout.workoutTrackData = selectedWorkout.workoutTrackData.filter(({ id }) => id !== deletedWorkoutTrackCollection?.id);
+                    onUpdate(Workout.copyFrom(Workout.copyFrom(selectedWorkout)));
+                }}
+                onClose={() => setSelectedWorkout(null)}
+            />
+        </>
+    );
+}
+
+export default Home;
