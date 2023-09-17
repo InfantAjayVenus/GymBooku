0cf21d0acb5c47cd12a56109c8ba2f7c477e6baf
Infant Ajay Venus <infantcsajay@outlook.com>
2023-09-17 08:33:19 +0530

    [util] chore: remove debug logs

--- src/utils/getAverage.ts
+++ src/utils/getAverage.ts
@@ -2,6 +2,5 @@ export default function getAverage(values: number[], roundOff = 2): number {
   if (values.length === 0) return 0;
   const sum = values.reduce((sumAcc, item) => sumAcc + item, 0);
 
-  console.log("DEBUG:GET_AVG:", values, sum);
   return parseFloat((sum / values.length).toFixed(roundOff));
 }
