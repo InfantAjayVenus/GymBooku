a6551909ad546acae9762f9cdbc47d5c4aa5619b
Infant Ajay Venus <infantcsajay@outlook.com>
2023-09-19 14:24:11 +0530

    [util] patch: adjust week start offset for getWeek

--- src/utils/getWeek.ts
+++ src/utils/getWeek.ts
@@ -1,5 +1,5 @@
 export default function getWeek(date: Date): number {
   const yearStart = new Date(date.getFullYear(), 0, 1);
   const daysDifference = (date.valueOf() - yearStart.valueOf()) / (24 * 60 * 60 * 1000);
-  return Math.ceil((daysDifference + yearStart.getDay() + 1) / 7);
+  return Math.ceil((daysDifference + yearStart.getDay()) / 7);
 }
