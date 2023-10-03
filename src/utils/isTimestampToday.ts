c9291867e55f8f971c7c241e119d9463545af8e4
Infant Ajay Venus <infantcsajay@outlook.com>
2023-10-03 22:13:03 +0530

    [util] patch: remove timezone offset for isTimstampToday

--- src/utils/isTimestampToday.ts
+++ src/utils/isTimestampToday.ts
@@ -1,6 +1,6 @@
 export default (timeStamp: Date): boolean => {
     const date = new Date();
-    const today = new Date(date.valueOf() - (date.getTimezoneOffset() * 60 * 1000));
+    const today = new Date(date.valueOf());
     
     return today.toDateString() === timeStamp.toDateString();
 }
