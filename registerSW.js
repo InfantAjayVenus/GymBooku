518dc1172a2514669feb592e570c35b537f99748
InfantAjayVenus <InfantAjayVenus@users.noreply.github.com>
2023-10-26 04:00:25 +0000

    deploy: 56326fc77b8865dadd2aafaa2f259fdbc06c932d

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://infantajayvenus.github.io/GymBooku/sw.js', { scope: 'https://infantajayvenus.github.io/GymBooku/' })})}
