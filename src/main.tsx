eea625030bd8d2e758d32be64a3125bfd78d2f5a
Infant Ajay Venus <infantcsajay@outlook.com>
2023-11-19 06:58:31 +0530

    [main] feat: wrap the app with config

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import ConfigProvider from './providers/ConfigProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
 )
 
-import "./registerServiceWorker";
+import "./registerServiceWorker"; 
+
