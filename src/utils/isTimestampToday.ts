7a74aefe2da9a205329fd2e483cc8a3c30197d1b
Infant Ajay Venus <infantcsajay@outlook.com>
2024-03-13 00:27:06 +0530

    [util] fix: change logic of istimestamptoday to use dayjs

--- src/utils/isTimestampToday.ts
+++ src/utils/isTimestampToday.ts
@@ -1,6 +1,6 @@
+import dayjs from "dayjs";
+
 export default (timeStamp: Date): boolean => {
-    const date = new Date();
-    const today = new Date(date.valueOf());
-    
-    return today.toDateString() === timeStamp.toDateString();
+    const today = dayjs();
+    return today.isSame(timeStamp, 'date') && today.isSame(timeStamp, 'month') && today.isSame(timeStamp, 'year');
 }
