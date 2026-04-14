fdb381c6a72dcb70a37e758d5412a7c30a4ad7f8
Infant Ajay Venus <infantcsajay@outlook.com>
2026-04-15 01:03:25 +0530

    chore: clean up playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,12 +25,8 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'http://127.0.0.1:5173',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    viewport: { width: 390, height: 844 },
   },
 
   /* Configure projects for major browsers */
